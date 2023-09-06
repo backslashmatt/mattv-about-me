944eaea7ef333b6aeef51516cd0db7871ce643fd
Matt V <matt.verry@outlook.com>
2023-09-05 19:10:01 -0700

    Leaving in api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,12 @@
 export const environment = {
   firebase: {
+    projectId: 'matt-v',
+    appId: '1:882775006249:web:4e382edad77a21b17d9038',
+    storageBucket: 'matt-v.appspot.com',
+    apiKey: 'AIzaSyAqdS9Y0qCsYLUm754IQioaGLsfdNYS6XY',
+    authDomain: 'matt-v.firebaseapp.com',
+    messagingSenderId: '882775006249',
+    measurementId: 'G-3LMSLDQQ8G',
   },
   production: false
 };
