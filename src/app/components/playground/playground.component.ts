5e83c1a446ac1cf0b8d65ecd53e137b80428d8b4
Matt V <matt.verry@outlook.com>
2023-09-12 06:40:53 -0700

    Signals form change with signals

--- src/app/components/playground/playground.component.ts
+++ src/app/components/playground/playground.component.ts
@@ -1,28 +1,54 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { FormsModule } from '@angular/forms';
+import { Component, effect, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { map, Observable, of } from 'rxjs';
+
+enum CustomerType {
+  Individual = 'Individual',
+  Business = 'Business',
+}
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
   styleUrls: ['./playground.component.scss'],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   standalone: true,
 })
 export class PlaygroundComponent implements OnInit {
-  customerService = { getCustomers: (id: string | null) => of(this.customers) };
-  router = inject(ActivatedRoute);
-
-  value?: number;
+  customerTypes = {
+    Individual: CustomerType.Individual,
+    Business: CustomerType.Business,
+  };
   customers = [
     { id: 1, name: 'John' },
     { id: 2, name: 'Jane' },
     { id: 3, name: 'Bob' },
     { id: 4, name: 'Alice' },
   ]
+  customerService = { getCustomers: (id: string | null) => of(this.customers) };
+  router = inject(ActivatedRoute);
+  fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    name: this.fb.control<string | undefined>(undefined),
+    type: this.fb.control<CustomerType | undefined>(undefined),
+    optionalEIN: this.fb.control<string | undefined>({ value: undefined, disabled: true }, Validators.required),
+  });
+
+  formTypeSignal = toSignal(this.form.controls.type.valueChanges);
+  formTypeEffect = effect(() => {
+    const type = this.formTypeSignal();
+    if (type === CustomerType.Business) {
+      this.form.controls.optionalEIN.enable();
+    } else {
+      this.form.controls.optionalEIN.disable();
+    }
+  })
+
+  value?: number;
 
   id$ = this.router.paramMap.pipe(map((params: ParamMap) => params.get('id')), takeUntilDestroyed());
   customers$?: Observable<{ id: number, name: string }[]>;
